2dcf861dbf1b930714e70f859a57fe71d1dcc634
Miguel A. Estrada <mae829@gmail.com>
2018-08-23 22:08:37 -0700

    Remove Google analytics from footer

--- footer.php
+++ footer.php
@@ -84,19 +84,5 @@
             </div>
         </footer><!--END FOOTER-->
 		<?php wp_footer(); ?>
-
-        
-        <!--GOOGLE ANALYTICS CODE HERE-->
-		<script type="text/javascript">
-            var _gaq = _gaq || [];
-            _gaq.push(['_setAccount', 'UA-13018822-6']);
-            _gaq.push(['_trackPageview']);
-          
-            (function() {
-              var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-              ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-              var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-            })();
-        </script>
     </body>
 </html>
