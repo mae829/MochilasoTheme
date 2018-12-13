ba410768a14c50a613b511d559b7c89e4f1a5f06
Miguel A. Estrada <mae829@gmail.com>
2018-12-12 20:48:07 -0800

    Use custom query for galleries in page-gallerires template instead of modifying the main WP query

--- page-galleries.php
+++ page-galleries.php
@@ -15,23 +15,27 @@ get_header();
 			<h2 class="page-title"><?php the_title(); ?></h2>
 
 			<?php
-				global $wp_query;
+			global $wp_query;
 
-				$wp_query = new WP_Query( 'post_type=gallery&post_status=publish&posts_per_page=-1' );
+			$galleries_args = array(
+				'post_type'      => 'gallery',
+				'post_status'    => 'publish',
+				'posts_per_page' => -1,
+			);
 
-			if ( $wp_query->have_posts() ) :
+			$galleries_query = new WP_Query( $galleries_args );
+
+			if ( $galleries_query->have_posts() ) :
 				?>
 
 				<ul id="galleries">
 				<?php
-				while ( $wp_query->have_posts() ) :
-					$wp_query->the_post();
+				while ( $galleries_query->have_posts() ) :
+					$galleries_query->the_post();
 					?>
 						<?php show_first_image(); ?>
 					<?php endwhile; ?>
 				</ul>
-
-				<?php // my_paginate_links(); ?>
 
 			<?php else : ?>
 				<p>Cannot find any posts at the moment.</p>
